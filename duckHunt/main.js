cfcb1c6242b3b6fa1694fa78793f8dd7faf99f09
Irving Andrade <irving.an.ve@gmail.com>
2018-09-05 15:43:39 -0500

    finalizar al jefe

--- duckHunt/main.js
+++ duckHunt/main.js
@@ -19,8 +19,11 @@ var pruebaloose=0 // no es necesario
 var bool = true
 var municiones = []
 var x = 0
-//clases
+var bosses = []
+var reds = []
+var contador=0
 
+//clases
 class Board {
   constructor(){
     this.x=0
@@ -34,8 +37,6 @@ class Board {
     }*/
     this.music = new Audio()
     this.music.src = 'thiller.mp3'
-    //this.music2 = new Audio()
-    //this.music2.src = 'shots.mp3'
   }
   draw(){
     ctx.drawImage(this.image,this.x,this.y,this.w,this.h)
@@ -50,13 +51,17 @@ class Vampire{
     this.x=Math.floor(Math.random()*canvas.width)
     this.y=400
     this.cachoX = 0
+    this.cachoXE = 0
     this.w=255/4
+    this.wE=242/7 
     this.horizontal = bool
     bool = !bool
     this.vertical = false
     this.image = new Image()
     this.image.src='Thebat.png'
     this.h=this.image.naturalHeight
+    this.image2 = new Image()
+    this.image2.src = 'explosion.png'
     //this.movimientos=[this.moveRight,this.moveLeft,this.moveDown,this.moveTop]
     //this.speed = Math.floor((Math.random()*5)+2)
   }
@@ -70,12 +75,18 @@ class Vampire{
     if(frames%10==0) this.changeSprite()
     ctx.drawImage(this.image,this.cachoX,0,this.w,this.h,this.x,this.y,this.w,this.h)
   }
-
   changeSprite(){
     this.cachoX+=this.w
     if(this.cachoX>this.image.naturalWidth-this.w) this.cachoX=0
   }
-
+  changeSpriteE(){
+  this.cachoXE+=this.wE
+  if(this.cachoXE>this.image2.naturalWidth-this.wE) this.cachoXE=0
+  }
+  deathAnimation(){
+      if(frames%20==0)this.changeSpriteE()
+      ctx.drawImage(this.image2,this.cachoXE,0,this.wE,this.image2.naturalHeight,this.x,this.y,this.wE,this.image2.naturalHeight)
+  }
   moveRight(){
     if(this.x<(canvas.width-this.w)){this.x+=2}
     else{this.horizontal=false}
@@ -127,7 +138,7 @@ class Municion{
   this.h=50
   this.vertical = false
   this.image = new Image()
-  this.image.src='gun2.jpeg'
+  this.image.src='gun2.png'
   }
   draw(){
 
@@ -163,13 +174,71 @@ class Municion{
             (this.y < yClick)&&
             (this.y+this.h >yClick);
   }
-}
+}//municion
+
+class Boss{
+  constructor(){
+  this.x = 350
+  this.y = 400  
+  this.w = 75
+  this.h = 75
+  this.vertical = true
+  this.horizontal = true
+  this.life = 20
+  }
+  draw(){
+    if(this.vertical === true){
+      this.moveTop()
+    }
+    if(this.vertical === false){
+      if(this.horizontal){this.moveRight()}
+      else{this.moveLeft()}
+    }
+
+    //Draw
+    ctx.fillStyle='red'
+    ctx.fillRect(this.x,this.y,this.w,this.h)
+  }
+  moveTop(){
+    if(this.y>=30)this.y-=5
+    else this.vertical=false
+  }
+  moveRight(){
+    if(this.x<(canvas.width-this.w)){this.x+=2}
+    else{this.horizontal=false}
+  }
+  moveLeft(){
+    if(this.x>=0){this.x-=2}
+    else{
+        this.horizontal=true
+    }
+  }
+} // Boss
+
+class redVamp{
+  constructor(x,y){
+    this.x = x
+    this.y = y
+    this.w = 30
+    this.h = 30
+  }
+  draw(){
+    this.moveDown()
+    //draw
+    ctx.fillStyle='red'
+    ctx.fillRect(this.x,this.y,this.w,this.h)
+  }
+  moveDown(){
+    this.y += 2
+  }
+} // redVamp
 
 //objetos
 var board = new Board()
+var boss = new Boss()
+bosses.push(boss)
 
 //funciones principales
-
 function start(){
   if(interval)return
   interval = setInterval(update,1000/60)
@@ -201,11 +270,11 @@ function update(){
   frames++
   ctx.clearRect(0,0,canvas.width,canvas.height)
   board.draw()  
-  if(frames%100===0 && frames<1000){
+  if(frames%100===0 && score<20){
     createEnemys()
     createEnemys()
   }
-  if(frames%100===0 && frames>1000){
+  if(frames%100===0 && score>19 && score<50){
     createEnemys()
     createEnemys()
     createEnemys()
@@ -214,6 +283,14 @@ function update(){
   if(frames%800===0){
     createMuniciones()
   }
+  /*if(frames>2800 && score>49){
+    createBoss()
+  }*/
+  if(frames%50 === 0 && frames>2700){
+    createRed()
+  }
+  if(score>49)drawBoss()
+  drawReds()
   drawMuniciones()
   drawEnemys()
   checkCollision()
@@ -223,7 +300,8 @@ function update(){
   deleteMunition()
   gameOver()
   xClick=null
-  yClick=null
+  yClick=
+  console.log(frames)
 } //update
 
 //funciones auxilires
@@ -241,6 +319,7 @@ function drawEnemys(){
 function checkCollision(){
   enemies.forEach(function(element){
     if(element.crashWith()){
+      element.deathAnimation()
       enemies.splice(enemies.indexOf(element),1)
       score++
     }
@@ -265,6 +344,7 @@ function createMuniciones(){
 
 function drawMuniciones(){
   municiones.forEach(function(element){
+    contador++
     element.draw()
   }) 
 }//drawMuniciones
@@ -282,7 +362,7 @@ function checkReload(){
 }
 
 function createBullets(){
-  for(i=0;i<3;i++){
+  for(i=0;i<5;i++){
     var bullet = new Bullet()
     if(i>0){
       bullet.x=x
@@ -302,17 +382,38 @@ function drawBullets(){
 } //drawBullets
 
 function deleteMunition(){
-  if (frames ===1400){
+  if (contador===600){
     municiones.pop()
+    contador = 0
   }
 }
-createBullets()
+/*function createBoss(){
+  boss = new Boss()
+  bosses.push(boss)
+
+}*/
+function drawBoss(){
+  bosses.forEach(function(element){
+  element.draw()
+  })
+}
+function createRed(){
+  red = new redVamp(bosses[0].x,bosses[0].y)
+  reds.push(red)
+}
+function drawReds(){
+  reds.forEach(function(element){
+  element.draw()
+  })
+}
+
+
+createBullets() // Inicia la instancias de las balas iniciales
 
 //listenners
 addEventListener("click",function(e){
   xClick = e.clientX-rect.left
   yClick = e.clientY-rect.top 
-  //board.music2.play()
   console.log(xClick)
   console.log(yClick)
 })
